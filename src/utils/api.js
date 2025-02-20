c0db8037dae8e498a49a5b0db7f148cf1bb4615d
RoyalTeeJ <tiagostar13@gmail.com>
2025-02-20 21:05:16 +0000

    add pagination api to api's needed and add new api to get articles by topic

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,17 +4,19 @@ const api = axios.create({
   baseURL: "https://royalteej-be-nc-news.onrender.com/api",
 });
 
-export const getArticles = () => {
-  return api.get("/articles").then((res) => res.data.article);
+export const getArticles = (page = 1) => {
+  return api
+    .get(`/articles?limit=10&page=${page}`)
+    .then((res) => res.data.article);
 };
 
 export const getArticleById = (article_id) => {
   return api.get(`/articles/${article_id}`).then((res) => res.data.article);
 };
 
-export const getCommentsByArticleId = (article_id) => {
+export const getCommentsByArticleId = (article_id, page = 1) => {
   return api
-    .get(`/articles/${article_id}/comments`)
+    .get(`/articles/${article_id}/comments?limit=10&page=${page}`)
     .then((res) => res.data.comments);
 };
 
@@ -38,4 +40,14 @@ export const postComment = (article_id, username, body) => {
 
 export const deleteComment = (comment_id) => {
   return api.delete(`/comments/${comment_id}`).then((res) => res.data);
+};
+
+export const getArticlesByTopic = (topic, page = 1) => {
+  return api
+    .get(`/articles?topic=${topic}&limit=10&page=${page}`)
+    .then((res) => res.data.article);
+};
+
+export const getTopics = () => {
+  return api.get(`/topics`).then((res) => res.data.topics);
 };
