2644780a125ccde014eb7f17390c515606e4fe6a
RoyalTeeJ <tiagostar13@gmail.com>
2025-02-20 20:58:32 +0000

    add new router for topics page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ArticlesList from "./components/ArticlesList";
 import ArticlePage from "./components/ArticlePage";
+import Navbar from "./components/NavBar";
+import TopicPage from "./components/TopicPage";
 import "./App.css";
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<ArticlesList />} />
-        <Route path="/articles/:article_id" element={<ArticlePage />} />
-      </Routes>
+      <div className="app-container">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<ArticlesList />} />
+          <Route path="/articles/:article_id" element={<ArticlePage />} />
+          <Route path="/topics/:topic_slug" element={<TopicPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
